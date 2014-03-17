a6f8a4362abdbeef084535c9c0a3d4aa5182099d
Keith Smiley <keithbsmiley@gmail.com>
2014-03-16 20:28:39 -0400

    First pass python

---
+++ python/gist.py
@@ -0,0 +1,19 @@
+import vim
+import argparse
+
+
+def main(args):
+    name = parser.parse_args(args.split())
+    parser.print_help()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--count', type=int)
+    parser.add_argument('--line1', type=int, metavar='start')
+    parser.add_argument('--line2', type=int, metavar='end')
+    private_default = vim.vars.get("gist_default_private", False) == 1
+    parser.add_argument('-P', '--public', action='store_true',
+                        dest='public', default=(not private_default))
+    parser.add_argument('-p', '--private', action='store_true',
+                        dest='private', default=private_default)
