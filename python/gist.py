7eeb440e5323c67dee23e98d2da1b36e129cff75
Keith Smiley <keithbsmiley@gmail.com>
2014-03-19 14:57:46 -0700

    Get text implementation

--- python/gist.py
+++ python/gist.py
@@ -1,5 +1,8 @@
 import argparse
+import os.path
+from gist.auth import user
 import vim
+import json
 
 
 def github_url(path):
@@ -9,7 +12,55 @@ def github_url(path):
 
 def main(args):
     name = parser.parse_args(args.split())
-    parser.print_help()
+    # print name
+    # parser.print_help()
+    data = {'public': name.public}
+    data['files'] = get_files(name)
+    vim.eval("inputsave()")
+    desc = vim.eval("inputdialog('Description: ')")
+    vim.eval("inputrestore()")
+    data['description'] = desc
+    # print json.dumps(data)
+    # TODO: request
+    u = user.User.from_netrc(url="github.com")
+    print u
+
+
+def get_files(args):
+    files = {}
+    b = vim.current.buffer
+    if args.count >= 0:
+        files[buffer_filename(b)] = text_from_buffer(b, args.line1, args.line2)
+        return files
+
+    bufs = [b]
+    if args.all:
+        bufs = []
+        for tab in vim.tabpages:
+            for win in tab.windows:
+                b = win.buffer
+                if not is_directory(b):
+                    bufs.append(b)
+
+    for b in bufs:
+        files[buffer_filename(b)] = text_from_buffer(b, 0, len(b))
+
+    return files
+
+
+def text_from_buffer(b, l1, l2):
+    # TODO: Must join strings in array by newline
+    if l1 > 0:
+        l1 -= 1
+    return b[l1:l2]
+
+
+def buffer_filename(b):
+    return os.path.basename(b.name)
+
+
+def is_directory(b):
+    return int(vim.eval("isdirectory(expand('%s'))" % b.name)) == 1
 
 
 if __name__ == "__main__":
@@ -20,5 +71,7 @@ if __name__ == "__main__":
     private_default = vim.vars.get("gist_default_private", False) == 1
     parser.add_argument('-P', '--public', action='store_true',
                         dest='public', default=(not private_default))
-    parser.add_argument('-p', '--private', action='store_true',
-                        dest='private', default=private_default)
+    parser.add_argument('-p', '--private', action='store_false',
+                        dest='public')
+    parser.add_argument('-a', '--all', action='store_true',
+                        default=False)
