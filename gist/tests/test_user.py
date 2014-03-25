0f8956c1d29ecf946daf78155d7a29f33ef66e15
Keith Smiley <keithbsmiley@gmail.com>
2014-03-25 01:22:59 -0400

    Test user

--- gist/tests/test_user.py
+++ gist/tests/test_user.py
@@ -1,13 +1,54 @@
+from gist.auth import user
+import tempfile
+import os
 import unittest
-from auth import user
+
+URL = "foobar.com"
+USERNAME = "foo"
+PASSWORD = "bar"
 
 
 class TestUsers(unittest.TestCase):
-    def test_netrc(self):
-        path = "/Users/ksmiley/netrc"
-        u = user.User.from_netrc(path, "github.com")
-        self.assertEqual(u.username, "foo")
+    def test_netrc_login(self):
+        text = "machine %s\n\tlogin %s\n\tpassword bar" % (URL, USERNAME)
+        name = make_temp(text)
+        u = user.User.from_netrc(name, URL)
+        os.remove(name)
+        self.assertEqual(u.username, USERNAME)
 
+    def test_netrc_username(self):
+        text = "machine %s\n\taccount %s\n\tpassword bar" % (URL, USERNAME)
+        name = make_temp(text)
+        u = user.User.from_netrc(name, URL)
+        os.remove(name)
+        self.assertEqual(u.username, USERNAME)
+
+    def test_string(self):
+        u = make_user()
+        self.assertRegexpMatches(u.__str__(), r".*Username: %s.*" % USERNAME)
+
+    def test_creation(self):
+        u = user.User(USERNAME, PASSWORD, URL)
+        self.assertEqual(u.username, USERNAME)
+        self.assertEqual(u.password, PASSWORD)
+        self.assertEqual(u.url, URL)
+
+
+def make_temp(text):
+    _, name = tempfile.mkstemp()
+    f = open(name, "w")
+    if not text:
+        text = "machine %s\n\tlogin %s\n\tpassword bar" % (URL, USERNAME)
+    f.write(text)
+    f.close()
+    return name
+
+
+def make_user():
+    name = make_temp(None)
+    u = user.User.from_netrc(name, URL)
+    os.remove(name)
+    return u
 
 if __name__ == "__main__":
     unittest.main()
