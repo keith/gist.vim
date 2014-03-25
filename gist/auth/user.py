10fbafc905b91ad26481da28877b1540a2f5fef1
Keith Smiley <keithbsmiley@gmail.com>
2014-03-25 01:22:38 -0400

    Cleanup and bugfix user script

--- gist/auth/user.py
+++ gist/auth/user.py
@@ -1,12 +1,12 @@
+"""
+Get users from the netrc credentials file for use with Github
+Maintained by: Keith Smiley <http://keith.so>
+"""
 import netrc
-import urllib2
+import urlparse
 
 
 class User(object):
-    username = None
-    password = None
-    url = None
-
     def __init__(self, username, password, url=None):
         self.username = username
         self.password = password
@@ -16,20 +16,15 @@ class User(object):
         return "Username: %s, URL: %s" % (self.username, self.url)
 
     @classmethod
-    def authorize(cls, username, password, url, save=True):
-        p = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        p.add_password(None, url, username, password)
-        urllib2.install_opener(urllib2.build_opener(
-                               urllib2.HTTPBasicAuthHandler(p)))
-        req = urllib2.Request(url)
-        pipe = urllib2.urlopen(req)
-        data = pipe.read()
-        print(data)
-
-    @classmethod
     def from_netrc(cls, path=None, url=None):
         rc = netrc.netrc(path)
-        creds = rc.authenticators(url)
+        parsed_url = urlparse.urlparse(url)
+        base_url = parsed_url.netloc
+        if not base_url:
+            base_url = parsed_url.path
+        if not base_url:
+            base_url = url
+        creds = rc.authenticators(base_url)
         if not creds:
             return None
         login, account, password = creds
