fc2c6547f7c331a6a474738f4fccaf84070a7209
Keith Smiley <keithbsmiley@gmail.com>
2014-03-25 01:23:13 -0400

    Cleanup, add, fix main script

--- gist/gist.py
+++ gist/gist.py
@@ -1,38 +1,125 @@
-import argparse
-import urllib2
-import base64
-import os.path
-import os
+"""
+Create gists straight from Vim
+Maintained by: Keith Smiley <http://keith.so>
+"""
 from gist.auth import user
-import vim
+import argparse
+import base64
 import json
-
-
-def github_url(path):
-    url = vim.vars.get("gist_base_url", "https://api.github.com/")
-    return os.path.join(url, "gists")
+import os
+import os.path
+import urllib2
+import vim
+import webbrowser
 
 
 def main(args):
-    name = parser.parse_args(args.split())
+    name, unknown = parser.parse_known_args(args.split())
+    data = data_for_args(name, unknown)
+
+    u = None
+    if not name.anonymous:
+        u = user.User.from_netrc(url=github_url(), path=netrc_path())
+        if not u:
+            print("No user in %s with machine %s" %
+                  (netrc_path(True), github_url()))
+            return
+    request = request_for_user(u)
+
+    try:
+        pipe = urllib2.urlopen(request, json.dumps(data))
+    except urllib2.HTTPError, e:
+        print(str(e.reason))
+        return
+
+    response = pipe.read()
+    pipe.close()
+    try:
+        j = json.loads(response)
+    except ValueError:
+        print("Failed to decode the response to JSON")
+    else:
+        open_url(j["html_url"])
+
+
+def netrc_path(default=False):
+    """
+    Return the settings based netrc path
+    """
+    path = vim.vars.get("gist_netrc_path", None)
+    if not path and default:
+        path = os.path.join(os.path.expanduser("~"), ".netrc")
+
+    return path
+
+
+def data_for_args(name, unknown):
+    """
+    Returns the hash for the given arguments
+    """
     data = {'public': name.public}
     data['files'] = get_files(name)
+    desc = ' '.join(unknown)
+    if not desc:
+        desc = get_description()
+    data['description'] = desc
+    return data
+
+
+def auth_for_user(u):
+    """
+    Formats the base64 string based on a user
+    """
+    return base64.standard_b64encode("%s:%s" % (u.username, u.password))
+
+
+def request_for_user(u):
+    """
+    Creates the URL request with the credentials
+    """
+    request = urllib2.Request(github_url("gists"))
+    if u:
+        auth = auth_for_user(u)
+        request.add_header("Authorization", "Basic %s" % auth)
+    return request
+
+
+def github_url(path=""):
+    """
+    Get the Github API URL
+    """
+    url = vim.vars.get("gist_base_url", "https://api.github.com/")
+    return os.path.join(url, path)
+
+
+def open_url(url):
+    """
+    Open the URL in the browser
+    """
+    vim.command("redraw!")
+    print(url)
+    switch = vim.vars.get("gist_should_open_url", 1) == 1
+    if switch:
+        webbrowser.open(url)
+
+
+def get_description():
+    """
+    Ask the user for a description of the gist
+    """
     vim.eval("inputsave()")
     desc = vim.eval("inputdialog('Description: ')")
     vim.eval("inputrestore()")
-    data['description'] = desc
-    u = user.User.from_netrc(url="github.com")
-    request = urllib2.Request(github_url("gists"))
-    auth = base64.standard_b64encode("%s:%s" % (u.username, u.password))
-    request.add_header("Authorization", "Basic %s" % auth)
-    pipe = urllib2.urlopen(request, json.dumps(data))
-    response = pipe.read()
-    pipe.close()
-    j = json.loads(response)
-    os.system("open " + j["html_url"])
+    vim.command("redraw!")
+    return desc
 
 
 def get_files(args):
+    """
+    Get the text from the files specified by the arguments
+    If there is a visual selection just that text is used
+    Otherwise if all is passed all buffers are used
+    """
     files = {}
     b = vim.current.buffer
     if args.count >= 0:
@@ -55,16 +142,27 @@ def get_files(args):
 
 
 def text_from_buffer(b, l1, l2):
+    """
+    Return the text from the given buffer between the given lines
+    This starts at a line before the given index to get all the text
+    From the array of lines, joined by a newline
+    """
     if l1 > 0:
         l1 -= 1
     return {'content': '\n'.join(b[l1:l2])}
 
 
 def buffer_filename(b):
+    """
+    Return just the filename of the given buffer
+    """
     return os.path.basename(b.name)
 
 
 def is_directory(b):
+    """
+    Return if the given buffer is a directory
+    """
     return os.path.isdir(b.name)
 
 
@@ -73,10 +171,12 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--count', type=int)
     parser.add_argument('--line1', type=int, metavar='start')
     parser.add_argument('--line2', type=int, metavar='end')
-    private_default = vim.vars.get("gist_default_private", False) == 1
+    private_default = vim.vars.get("gist_default_private", 0) == 1
     parser.add_argument('-P', '--public', action='store_true',
                         dest='public', default=(not private_default))
     parser.add_argument('-p', '--private', action='store_false',
                         dest='public')
     parser.add_argument('-a', '--all', action='store_true',
+                        default=False)
+    parser.add_argument('-A', '--anonymous', action='store_true',
                         default=False)
