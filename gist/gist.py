09e61d9cf753b483d2104d347527a7bdabc1ec65
Keith Smiley <keithbsmiley@gmail.com>
2017-08-06 22:03:43 -0700

    Add ability to copy a URL

--- gist/gist.py
+++ gist/gist.py
@@ -50,6 +50,9 @@ def main(args):
 
     url = response_json["html_url"]
     open_url(url, browser=name.open_browser)
+    if name.copy_url:
+        copy_url(url)
+
     save_url(url)
 
 
@@ -125,6 +128,40 @@ def open_url(url, browser=False):
         webbrowser.open_new_tab(url)
 
 
+def copy_last_url():
+    """
+    Get the last URL from vim and open it
+    """
+    url = vim.vars.get("gist_last_gist_url", "")
+    copy_url(url)
+
+
+def copy_url(url):
+    """
+    Copies the given URL to the yank register
+    """
+    if not url:
+        print("No Gist URL")
+        return
+
+    vim.command("let @%s = \"%s\"" % (_get_yank_register(), url))
+
+
+def _get_yank_register():
+    """
+    Returns the symbol for the current yank register
+    """
+    register = vim.vars.get("gist_yank_register", "")
+    if register:
+        return register
+
+    clipboard = vim.vars.get("clipboard", "")
+    if clipboard == "unnamedplus":
+        return "+"
+    else:
+        return "*"
+
+
 def get_description():
     """
     Ask the user for a description of the gist
@@ -195,6 +232,7 @@ if __name__ == "__main__":
     parser.add_argument('--line2', type=int, metavar='end')
     private_default = vim.vars.get("gist_default_private", 0) == 1
     open_default = vim.vars.get("gist_open_url", 1) == 1
+    copy_default = vim.vars.get("gist_copy_url", 0) == 1
     parser.add_argument('-P', '--public', action='store_true',
                         dest='public', default=(not private_default))
     parser.add_argument('-p', '--private', action='store_false',
@@ -203,3 +241,5 @@ if __name__ == "__main__":
                         default=False)
     parser.add_argument('-o', '--open', action='store_true',
                         dest='open_browser', default=open_default)
+    parser.add_argument('-y', '--yank', action='store_true',
+                        dest='copy_url', default=copy_default)
